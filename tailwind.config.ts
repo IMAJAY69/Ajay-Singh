97c09746893f0cb592186da2c4c1089ea737caf2
IMAJAY69 <assikary@gmail.com>
2026-05-29 12:56:17 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: "#050505",
+        surface: "#111111",
+        neonRed: "#FF003C",
+        electricBlue: "#00F0FF",
+        goldAccent: "#D4AF37",
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Oswald', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
